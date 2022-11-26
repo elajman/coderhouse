3ed4f8739a48deb48e153070864a817c2fd8373d
elajman <elajman2000@gmail.com>
2022-11-26 10:26:51 -0300

    validacion formularios

--- plugin/script.js
+++ plugin/script.js
@@ -1,74 +1,53 @@
-const name = document.getElementById('username')
-const email = document.getElementById('email')
-const form = document.getElementById('form')
+const name = document.getElementById('username');
+const form = document.getElementById('form');
 
+    const inputmail = document.querySelector('#email');
+    const inputname = document.querySelector('#username');
+    const fechaServicio = document.getElementById('fecha')
+   
 
-function alerta(){
- 
-    if (username.value === '' || email.value === '' || username.value == null || email.value == null){
-        alert('Por favor ingrese Nombre y correo Valido, Gracias')
-    }   
-    else{
-        alert(`Gracias ${username.value} por suscribirse a nuestro Newsletter`)
+    function veryfyEmail(){
+
+    if (!inputmail.value.match(/[^\s@]+@[^\s@]+\.[^\s@]+/gi) || inputname.value === '' || inputname.value == null) {
+        alert(`Por favor complete Nombre y Correo`);
+        return false;
+
+      }else {
+        alert(`Gracias ${inputname.value} por suscribirse a nuestro Newsletter`);
         window.location.href = 'https://elajman.github.io/coderhouse/paginas/promociones.html'
-    }       
-}
-const nombre = document.getElementById('nombre')
-const correo = document.getElementById('correo')
-const fecha = document.getElementById('fecha')
-const telefono = document.getElementById('telefono')
-
-
-function contacto(){
-    if (nombre.value === '' || correo.value === '' || nombre.value == null || correo.value == null){
-        alert('Por favor ingrese Nombre y correo Valido, Gracias')
-    }   
-    else{
-        alert(`Gracias ${nombre.value} pronto un asesor se pondra en contacto`)
-        window.location.href = 'https://elajman.github.io/coderhouse/paginas/promociones.html'
-    }       
-}
-function ubicacion(){
-    if (nombre.value === '' || email.value === '' || nombre.value == null || email.value == null || telefono.value === ''|| fecha.value ==='' || telefono.value == null || fecha.value == null){
-        alert('Por favor ingrese Nombre, correo, telefono y fecha de visita Validos, Gracias')
-    }   
-    else{
-        alert(`Gracias ${nombre.value} el dia ${fecha.value} lo estaremos esperando, Gracias`)
-        window.location.href = 'https://elajman.github.io/coderhouse/paginas/promociones.html'
-    }       
-}
-
-
-
-const nombreServicio = document.getElementById('nombreServicio')
-const correoServicio = document.getElementById('correoServicio')
-const fechaServicio = document.getElementById('fechaServicio')
-
-
-function validarServicio(){
-    if (nombreServicio.value === '' || correoServicio.value === '' || nombreServicio.value == null || correoServicio.value == null || fechaServicio.value === ''|| fechaServicio.value == null){
-        alert('Por favor ingrese Nombre, correo y fecha de servicio Validos, Gracias')
-    }   
-    else{
-        alert(`Gracias ${nombreServicio.value} el dia ${fechaServicio.value} lo estaremos esperando para realizar servicio a su reloj, Gracias`)
-        window.location.href = 'https://elajman.github.io/coderhouse/paginas/promociones.html'
-    }       
-}
-
-/*Validacion con Mensaje*/
-/*form.addEventListener('submit', (e) => {
-    let meesage = []
-    if (username.value === '' || username.value == null) {
-        meesage.push('Es requerido un nombre y')
+        return true;
+      }
     }
-    else{
-        meesage.push = alert(`Gracias ${username.value} por susucribirse al Newsletter`)
-       
-    }
+    function veryfyServicio(){
+
+        if (!inputmail.value.match(/[^\s@]+@[^\s@]+\.[^\s@]+/gi) || inputname.value === '' || inputname.value == null || fecha.value === ''|| fecha.value == null) {
+            alert(`Por favor complete Nombre, Correo y fecha de Service`);
+            return false;
     
-    if (meesage.length > 0) {
-        e.preventDefault()
-        errorElement.innerText = meesage.join(', ')
+          }else {
+            alert(`Gracias ${inputname.value} el dia ${fecha.value} lo estaremos esperando para realizar servicio a su reloj, Gracias`)
+            window.location.href = 'https://elajman.github.io/coderhouse/paginas/promociones.html'
+            return true;
+          }
     }
+    function veryContacto(){
+
+        if (!inputmail.value.match(/[^\s@]+@[^\s@]+\.[^\s@]+/gi) || inputname.value === '' || inputname.value == null) {
+            alert(`Por favor complete Nombre y Correo`);
+            return false;
     
-})*/
+          }else {
+            alert(`Gracias ${inputname.value} Nos pondremos en contacto a la brevedad`);
+            window.location.href = 'https://elajman.github.io/coderhouse/paginas/promociones.html'
+            return true;
+          }
+        }
+
+
+
+
+
+
+
+
+
